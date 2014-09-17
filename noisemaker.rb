fea33c592a990103b36933b76ae2e89f9e322001
Clay Wells <clayw@upenn.edu>
2014-09-17 16:52:40 -0400

    adding initial ruby port.. not much at the moment.

---
+++ noisemaker.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+# noisemaker.rb
+
+# porting noisemaker.py to Ruby
+
+# using nokogiri to parse HTML
+require 'nokogiri'
+
+
+class GoogleLinks
+
+    def initialize()
+    	@links = []
+    	@searchword = ""
+
+	def handle_starttag(self, tag, attrs)
+		# TODO
+
+	end
+
+	def printLinks(self)
+		# TODO
+
+	end
+
+	def spiderRandomLink(self)
+		# TODO
+
+	end
+
+	def randomAgentString(self)
+		# TODO
+
+	end
+
+end
+
+class getWord:
+    wordfile = "/usr/share/dict/words"
+
+    def initialize()
+	end
+
+	  
+    def getWord(self):
+	end
+
+	  
+    def getCount(self)
+    end
+
+end
